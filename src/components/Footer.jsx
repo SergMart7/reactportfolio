e7ba85dae4b6084cc1a294abb56fec117935b61b
Sergio Martinez <sergmart7@Sergios-MacBook-Air.local>
2024-10-15 21:50:26 -0600

    bug fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 function Footer() {
     return (
       <footer>
-        <p>&copy; 2024 Y03/our Name</p>
+        <p>&copy; 2024 Sergio Martinez</p>
+        <p>Fait avec amour❤️</p>
         <a href="https://github.com/SergMart7">GitHub</a>
         {/* I know this linkedin link will not work until I create an account, but it serves to show my backup page in case no route is found */}
         <a href="www.linkedin.com/in/sergio-martinez-933426303">LinkedIn</a>
